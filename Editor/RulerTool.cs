3ae39cfa01df1cfd843dedb638f055a3791d6518
mattdevv <94596138+mattdevv@users.noreply.github.com>
2026-03-02 16:07:10 +1030

    Fix tool for release

--- Editor/RulerTool.cs
+++ Editor/RulerTool.cs
@@ -3,9 +3,10 @@ using System.Reflection;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.EditorTools;
+using UnityEditor.ShortcutManagement;
 using Object = UnityEngine.Object;
 
-namespace mattdevv
+namespace mattdevv.Editor
 {
     internal static class InternalEditorTools
     {
@@ -37,14 +38,17 @@ namespace mattdevv
     [EditorTool("Ruler Tool")]
     public class RulerTool : EditorTool
     {
+        [Shortcut("Activate QuickRuler", KeyCode.Q, ShortcutModifiers.Control)]
+        static void ToolShortcut()
+        {
+            ToolManager.SetActiveTool<RulerTool>();
+        }
+        
         public override GUIContent toolbarIcon => new GUIContent(
             AssetDatabase.LoadAssetByGUID<Texture>(new GUID("e23c67c3e6dbf794081ed9fc1cdc588f")), "Ruler Tool");
 
-        private bool isDragging;
-        private bool foundStart;
-        private bool foundEnd;
-        private Vector3 startPoint;
-        private Vector3 endPoint;
+        private List<Vector3> nodes = new ();
+        private Vector3 mouseNode;
 
         private bool foundVertex;
         private Vector3 vertexPosition;
@@ -52,22 +56,25 @@ namespace mattdevv
         private const float markerSize = 0.05f;
         
         private static readonly List<Object> picks = new (16);
-        
-        public override void OnActivated()
-        {
-            ResetRuler();
-        }
 
-        private bool ProjectPointInWorld(Vector2 guiPoint, bool tryVertexSnap, ref Vector3 result)
+        private GUIStyle boxStyle;
+        private GUIStyle textStyle;
+
+        private void ProjectPointInWorld(Vector2 guiPoint, bool tryVertexSnap, out Vector3 result)
         {
             // Try to project onto a mesh vertex
             if (tryVertexSnap && HandleUtility.FindNearestVertex(guiPoint, out Vector3 vertex))
             {
-                foundVertex = true;
-                vertexPosition = vertex;
-                result = vertex;
-                
-                return true;
+                const float pixelRadius = 50;
+                Vector2 point = HandleUtility.WorldToGUIPoint(vertex);
+                if (Vector2.SqrMagnitude(point - guiPoint) < pixelRadius * pixelRadius)
+                {
+                    foundVertex = true;
+                    vertexPosition = vertex;
+                    result = vertex;
+
+                    return;
+                }
             }
 
             // store that the latest projection was not to a vertex
@@ -83,7 +90,7 @@ namespace mattdevv
                     if (InternalEditorTools.IntersectRayMesh(HandleUtility.GUIPointToWorldRay(guiPoint), meshFilter, out RaycastHit hit))
                     {
                         result = hit.point;
-                        return true;
+                        return;
                     }
                 }
             }
@@ -97,35 +104,34 @@ namespace mattdevv
             if (pivotPlane.Raycast(cameraRay, out float d))
             {
                 result = cameraRay.GetPoint(d);
-                return true;
+                return;
             }
 
-            return false;
+            result = cameraRay.origin;
         }
 
         public override void OnToolGUI(EditorWindow window)
         {
+            if (boxStyle == null)
+            {
+                boxStyle = new GUIStyle(GUI.skin.box);
+                textStyle = new GUIStyle(EditorStyles.boldLabel) {
+                    normal = { textColor = Color.white }
+                }; 
+            
+            }
+            
             Event e = Event.current;
             
             if (e.type == EventType.Repaint)
             {
+                DrawRulers();
+                
                 // highlight the current vertex being snapped to
                 if (foundVertex)
                 {
                     Handles.color = Color.white;
                     Handles.RectangleHandleCap(0, vertexPosition, SceneView.lastActiveSceneView.camera.transform.rotation, markerSize, EventType.Repaint);
-                }
-                
-                // Draw ruler if dragging
-                if (isDragging && foundEnd)
-                {
-                    DrawRuler(startPoint, endPoint);
-                }
-                // else highlight the position under the cursor
-                else if (foundStart)
-                {
-                    Handles.color = Color.yellow;
-                    Handles.DrawWireDisc(startPoint, SceneView.lastActiveSceneView.camera.transform.forward, markerSize * 2f);
                 }
             }
             else if (e.type == EventType.Layout)
@@ -134,35 +140,47 @@ namespace mattdevv
             }
             else
             {
-                if (isDragging)
-                {
-                    if (ProjectPointInWorld(e.mousePosition, e.shift, ref endPoint))
-                    {
-                        foundEnd = true;
-                        SceneView.RepaintAll();
-                    }
-                }
-                else
-                { 
-                    if (ProjectPointInWorld(e.mousePosition, e.shift, ref startPoint))
-                    {
-                        foundStart = true;
-                        SceneView.RepaintAll();
-                    }
-                }
+                ProjectPointInWorld(e.mousePosition, e.shift, out mouseNode);
+                SceneView.RepaintAll();
                 
                 HandleInput(e);
             }
+            
+            Window();
+        }
+        
+        private Rect windowRect = new Rect(20, 20, 260, 140);
+        private void Window()
+        {
+            Handles.BeginGUI();
+
+            windowRect = GUI.Window(
+                123456,
+                windowRect,
+                DrawWindow,
+                "My Tool Settings"
+            );
+
+            Handles.EndGUI();
+        }
+        
+        void DrawWindow(int id)
+        {
+            EditorGUILayout.FloatField("Radius", 0);
+            EditorGUILayout.Toggle("Advanced", false);
+            GUILayout.Button("Reset");
+
+            GUI.DragWindow();
         }
 
         private void HandleInput(Event e)
         {
-            // check if shift was released
+            // check if (final) shift key was released
             if (e.type == EventType.KeyUp && (e.keyCode == KeyCode.LeftShift || e.keyCode == KeyCode.RightShift) && !e.shift)
             {
                 foundVertex = false;
             }
-            else if (isDragging)
+            else if (nodes.Count > 0)
             {
                 if (e.type == EventType.MouseLeaveWindow)
                 {
@@ -178,14 +196,23 @@ namespace mattdevv
                     
                     ResetRuler();
                 }
+                else if (e.type == EventType.MouseDown && e.button == 1)
+                {
+                    e.Use();
+                    
+                    // check that ruler has length
+                    if (Vector3.SqrMagnitude(mouseNode - nodes[^1]) > 0.00005f)
+                        nodes.Add(mouseNode);
+                }
             }
             else
             {
                 if (e.type == EventType.MouseDown && e.button == 0)
                 {
-                    // Lock start point
-                    isDragging = true;
                     e.Use();
+                    
+                    // Lock start point
+                    nodes.Add(mouseNode);
                 }
             }
             
@@ -193,13 +220,11 @@ namespace mattdevv
 
         private void ResetRuler()
         {
-            isDragging = false;
-            foundEnd = false;
-            foundStart = false;
+            nodes.Clear();
             foundVertex = false;
         }
 
-        private void DrawRulerLine(Vector3 a, Vector3 b, Color colorA, Color colorB, int segments = 1)
+        private void DrawSegmentLine(Vector3 a, Vector3 b, int segments, Color colorA, Color colorB)
         {
             float segmentCount = segments; //5 * Mathf.Log((a - b).magnitude + 1) + 3;
             Vector3 segment = (b - a) * (1f / segmentCount);
@@ -222,11 +247,49 @@ namespace mattdevv
             }
         }
 
-        private void DrawRuler(Vector3 a, Vector3 b)
+        private void DrawRulers()
         {
             var camera = SceneView.lastActiveSceneView.camera;
             var cameraPos = camera.transform.position;
+            var cameraDir = camera.transform.forward;
 
+            if (nodes.Count > 0)
+            {
+                for (int i = 1; i < nodes.Count; i++)
+                {
+                    if (i == 1)
+                        DrawRuler(nodes[i - 1], nodes[i], cameraPos, cameraDir, true, false);
+                    else
+                        DrawRuler(nodes[i - 1], nodes[i], cameraPos, cameraDir, false, false);
+                }
+
+                if (Vector3.SqrMagnitude(nodes[^1] - mouseNode) > 0.00005f)
+                {
+                        DrawRuler(nodes[^1], mouseNode, cameraPos, cameraDir, nodes.Count < 2, true);
+                }
+                else if (nodes.Count == 1)
+                {
+                    Handles.color = Color.yellow;
+                    Handles.SphereHandleCap(0, mouseNode, Quaternion.identity, markerSize, EventType.Repaint);
+                }
+
+                if (nodes.Count > 1)
+                {
+                    float totalLength = Vector3.Distance(nodes[^1], mouseNode);
+                    for (int i = 1; i < nodes.Count; i++)
+                        totalLength += Vector3.Distance(nodes[i - 1], nodes[i]);
+                    
+                    DrawText("Total: " + totalLength.ToString("F3") + " m", Event.current.mousePosition + new Vector2(0, -15));
+                }
+            }
+            else
+            {
+                Handles.SphereHandleCap(0, mouseNode, Quaternion.identity, markerSize, EventType.Repaint);
+            }
+        }
+
+        private void DrawRuler(in Vector3 a, in Vector3 b, in Vector3 cameraPos, in Vector3 cameraDir, bool startArrow, bool endArrow)
+        {
             // shift render points towards camera by a percentage to prevent z-fighting
             var lineStart = cameraPos + 0.99f * (a - cameraPos);
             var lineEnd = cameraPos + 0.99f * (b - cameraPos);
@@ -236,80 +299,106 @@ namespace mattdevv
             Vector3 lineDir = (b - a).normalized;
 
             Vector3 handlePosA = lineStart + lineDir * (calcSize * 0.7f);
-            Quaternion handleRotA = Quaternion.LookRotation(-lineDir);
+            Quaternion handleRotA = Quaternion.LookRotation(-lineDir, cameraDir);
             Vector3 handlePosB = lineEnd - lineDir * (calcSize * 0.7f);
-            Quaternion handleRotB = Quaternion.LookRotation(lineDir);
+            Quaternion handleRotB = Quaternion.LookRotation(lineDir, cameraDir);
             
-            lineStart += lineDir * (calcSize * 1.2f);
-            lineEnd -= lineDir * (calcSize * 1.2f);
+            lineStart += lineDir * (startArrow ? calcSize * 1.2f : calcSize * 0.5f);
+            lineEnd -= lineDir * (endArrow ? calcSize * 1.2f : calcSize * 0.5f);
 
             // change order of drawing so that objects overlap correctly
             if ((handlePosA - cameraPos).sqrMagnitude < (handlePosB - cameraPos).sqrMagnitude)
             {
                 Handles.zTest = UnityEngine.Rendering.CompareFunction.Greater;
                 Handles.color = Color.red;
-                Handles.ConeHandleCap(0, handlePosB, handleRotB, calcSize, EventType.Repaint);
+                if (endArrow)
+                    Handles.ConeHandleCap(0, handlePosB, handleRotB, calcSize, EventType.Repaint);
+                else                    
+                    Handles.SphereHandleCap(0, b, handleRotB, calcSize, EventType.Repaint);
                 Handles.DrawLine(lineStart, lineEnd);
-                Handles.ConeHandleCap(0, handlePosA, handleRotA, calcSize, EventType.Repaint);
-
+                if (startArrow)
+                    Handles.ConeHandleCap(0, handlePosA, handleRotA, calcSize, EventType.Repaint);
+                else
+                    Handles.SphereHandleCap(0, a, handleRotA, calcSize, EventType.Repaint);
+                
                 Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
                 Handles.color = Color.yellow;
-                Handles.ConeHandleCap(0, handlePosB, handleRotB, calcSize, EventType.Repaint);
-                DrawRulerLine(lineStart, lineEnd, new Color(1,1,0,1), new Color(0, 0, 0, 1), 20);
+                if (endArrow)
+                    Handles.ConeHandleCap(0, handlePosB, handleRotB, calcSize, EventType.Repaint);
+                else                    
+                    Handles.SphereHandleCap(0, b, handleRotB, calcSize, EventType.Repaint);
+                DrawSegmentLine(lineStart, lineEnd, 20, new Color(1,1,0,1), new Color(0, 0, 0, 1));
                 Handles.color = Color.yellow;
-                Handles.ConeHandleCap(0, handlePosA, handleRotA, calcSize, EventType.Repaint);
+                if (startArrow)
+                    Handles.ConeHandleCap(0, handlePosA, handleRotA, calcSize, EventType.Repaint);
+                else
+                    Handles.SphereHandleCap(0, a, handleRotA, calcSize, EventType.Repaint);
             }
             else
             {
                 // draw behind objects
                 Handles.zTest = UnityEngine.Rendering.CompareFunction.Greater;
                 Handles.color = Color.red;
-                Handles.ConeHandleCap(0, handlePosA, handleRotA, calcSize, EventType.Repaint);
+                if (startArrow)
+                    Handles.ConeHandleCap(0, handlePosA, handleRotA, calcSize, EventType.Repaint);
+                else
+                    Handles.SphereHandleCap(0, a, handleRotA, calcSize, EventType.Repaint);
                 Handles.DrawLine(lineStart, lineEnd);
-                Handles.ConeHandleCap(0, handlePosB, handleRotB, calcSize, EventType.Repaint);
+                if (endArrow)
+                    Handles.ConeHandleCap(0, handlePosB, handleRotB, calcSize, EventType.Repaint);
+                else                    
+                    Handles.SphereHandleCap(0, b, handleRotB, calcSize, EventType.Repaint);
                 
                 // draw in front of objects
                 Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
                 Handles.color = Color.yellow;
-                Handles.ConeHandleCap(0, handlePosA, handleRotA, calcSize, EventType.Repaint);
-                DrawRulerLine(lineStart, lineEnd, new Color(1,1,0,1), new Color(0, 0, 0, 1), 20);
+                if (startArrow)
+                    Handles.ConeHandleCap(0, handlePosA, handleRotA, calcSize, EventType.Repaint);
+                else
+                    Handles.SphereHandleCap(0, a, handleRotA, calcSize, EventType.Repaint);
+                DrawSegmentLine(lineStart, lineEnd, 20, new Color(1,1,0,1), new Color(0, 0, 0, 1));
                 Handles.color = Color.yellow;
-                Handles.ConeHandleCap(0, handlePosB, handleRotB, calcSize, EventType.Repaint);
+                if (endArrow)
+                    Handles.ConeHandleCap(0, handlePosB, handleRotB, calcSize, EventType.Repaint);
+                else                    
+                    Handles.SphereHandleCap(0, b, handleRotB, calcSize, EventType.Repaint);
             }
+            
             // reset for more drawing
             Handles.zTest = UnityEngine.Rendering.CompareFunction.Always;
             
-            // Distance label in screen space
-            float distance = Vector3.Distance(a, b);
-            Vector2 guiPoint = (HandleUtility.WorldToGUIPoint(a) + HandleUtility.WorldToGUIPoint(b)) * 0.5f;
             
-            var cameraTransform = camera.transform;
-            Vector3 cameraDir = cameraTransform.forward;
-            Vector3 cameraPlanePoint = cameraTransform.position + cameraDir * SceneView.lastActiveSceneView.camera.nearClipPlane;
+            Vector3 cameraPlanePoint = cameraPos + cameraDir * SceneView.lastActiveSceneView.camera.nearClipPlane;
             
             // only draw the label if the points are in front of the camera
             if (Vector3.Dot(cameraDir, a - cameraPlanePoint) > 0 && Vector3.Dot(cameraDir, b - cameraPlanePoint) > 0)
             {
-                Handles.BeginGUI();
-                GUIStyle style = new GUIStyle(EditorStyles.boldLabel)
-                {
-                    normal = { textColor = Color.white }
-                };
+                // Distance label in screen space
+                Vector2 guiPoint = (HandleUtility.WorldToGUIPoint(a) + HandleUtility.WorldToGUIPoint(b)) * 0.5f;
+                
+                float distance = Vector3.Distance(a, b);
                 string text = distance.ToString("F3") + " m";
-                Vector2 size = style.CalcSize(new GUIContent(text));
-                Rect rect = new Rect(guiPoint.x - size.x * 0.5f, guiPoint.y - size.y * 0.5f, size.x, size.y);
+                
+                DrawText(text, guiPoint);
+            }
+        }
 
+        public void DrawText(in string text, in Vector2 center)
+        {
+            Vector2 size = textStyle.CalcSize(new GUIContent(text));
+            Rect rect = new Rect(center.x - size.x * 0.5f, center.y - size.y * 0.5f, size.x, size.y);
+                
+            Handles.BeginGUI();
+            {
                 // draw twice for extra darkness
-                var roundedStyle = new GUIStyle(GUI.skin.box);
                 GUI.color = new Color(0, 0, 0, .75f);
-                EditorGUI.LabelField(rect, GUIContent.none, roundedStyle);
-                EditorGUI.LabelField(rect, GUIContent.none, roundedStyle);
+                EditorGUI.LabelField(rect, GUIContent.none, boxStyle);
+                EditorGUI.LabelField(rect, GUIContent.none, boxStyle);
 
                 GUI.color = Color.white;
-                GUI.Label(rect, text, style);
-
-                Handles.EndGUI();
+                EditorGUI.LabelField(rect, text, textStyle);
             }
+            Handles.EndGUI(); 
         }
     }
 }
